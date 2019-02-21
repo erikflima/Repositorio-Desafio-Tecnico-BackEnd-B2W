b5c0f9e49629fc02b6c29300366cb7bc402c921d
erikflima <erik.alves253@gmail.com>
2019-02-21 19:00:19 -0300

    Commit inicial

---
+++ src/main/java/com/erik/desafiotecnico/DesafioTecnicoJavaB2WApplication.java
@@ -0,0 +1,13 @@
+package com.erik.desafiotecnico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTecnicoJavaB2WApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTecnicoJavaB2WApplication.class, args);
+	}
+
+}
